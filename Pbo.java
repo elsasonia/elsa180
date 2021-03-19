db60bc2508d6cf7d8218cd1054119192da0879e2
elsa180 <80958830+elsasonia@users.noreply.github.com>
2021-03-19 20:25:25 +0700

    Add files via upload

---
+++ Pbo.java
@@ -0,0 +1,15 @@
+package pbo;
+
+public class Pbo {
+
+    public static void main(String[] args) {
+       
+        double gallons;
+        double litters;
+
+        gallons = 10;
+        litters = gallons * 3.7854;
+        System.out.println("jadi" + litters + "Litter" + gallons+ "gallons");
+    }
+}
+
